558aabc6a67d39d4e34d6d13cf106fc500c0e356
Frederick Ollinger <follinge@gmail.com>
2018-03-28 22:20:25 -0700

    add code

---
+++ README.md
@@ -0,0 +1,5 @@
+# Qt5 Bitcoin Price Downloader
+
+Create a class to download bitcoin current price.
+
+For use in regular bitcoin, eventually.
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QDebug>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QUrl>
+
+int main( int argc, char **argv )
+{
+    QApplication a( argc, argv );
+
+    return a.exec();
+}
